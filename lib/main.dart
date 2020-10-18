86f96aad57fe973dae771283448f07c96a4fa37a
MMattLeeE <mmattleee@gmail.com>
2020-10-18 18:03:43 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('flutter app'),
+      ),
+      body: Center(
+        child: Text('hello'),
+      ),
+    );
+  }
+}
